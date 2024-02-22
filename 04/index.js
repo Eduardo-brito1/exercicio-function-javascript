84b7558a20c9df9fc96416a7188ed9aff6326cdd
Eduardo-Brito1 <kabisashi2@gmail.com>
2024-02-22 16:38:16 -0300

    adicionando exercicio b completo

--- 04/index.js
+++ 04/index.js
@@ -1,5 +1,15 @@
 const contaBancaria = {
     nome: "Maria",
     saldo: 0,
-    historicos: []
+    historicos: [],
+    depositar: function depositar(valor) {
+        this.saldo = valor + this.saldo
+        this.historicos.push({
+            tipo: 'deposito',
+            valor: valor
+        })
+    }
 }
+
+contaBancaria.depositar(1000)
+console.log(contaBancaria)
